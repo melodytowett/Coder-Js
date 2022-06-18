e532c210ab0b7ad3d1087dc375ce1fac4175ad46
melodytowett <melodytowett99@gmail.com>
2022-06-18 12:08:40 +0300

    coderslang tasks

---
+++ task70/solution.js
@@ -0,0 +1,9 @@
+
+let salary = 100;
+let odlSalary = salary;
+console.log(odlSalary)
+console.log(salary)
+
+salary = 20000;
+console.log(odlSalary)
+console.log(salary)
---
+++ task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const sum = 10;
+const arr = 3;
+const pi = 3.17;
+const name = 'mel';
+const schl = 'Moringa';
